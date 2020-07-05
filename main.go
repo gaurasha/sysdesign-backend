944f16f50ac75e121d003048b879b7b78e834bf5
Gaurav Sharma <gaurasha.gs@gmail.com>
2020-07-05 12:38:14 +0530

    First commit

--- main.go
+++ main.go
@@ -5,7 +5,63 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/websocket"
 )
+
+var clients = make(map[*websocket.Conn]bool) // connected clients
+var broadcast = make(chan Message)           // broadcast channel
+
+// Configure the upgrader
+var upgrader = websocket.Upgrader{}
+
+// Define our message object
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// Upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Make sure we close the connection when the function returns
+	defer ws.Close()
+	// Register our new client
+	clients[ws] = true
+	for {
+		var msg Message
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		// Send the newly received message to the broadcast channel
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// Grab the next message from the broadcast channel
+		msg := <-broadcast
+		fmt.Printf("msg recieved : %s \n", msg)
+		// Send it out to every client that is currently connected
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World")
@@ -13,7 +69,15 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/", hello)
+	// Create a simple file server
+	fs := http.FileServer(http.Dir("../public"))
+	http.Handle("/", fs)
+	// Configure websocket route
+	http.HandleFunc("/ws", handleConnections)
+	// Start listening for incoming chat messages
+	go handleMessages()
+
+	http.HandleFunc("/hello", hello)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
